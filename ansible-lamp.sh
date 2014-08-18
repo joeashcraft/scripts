751c2fdb1d22c0b3145cdcd0b6ed6a430d7b0baf
tbr0 <taylor.broesche@rackspace.com>
2014-08-18 13:35:07 -0500

    Fixing Debian ansible script.

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -2,7 +2,7 @@ if [ -f /etc/redhat-release ]; then
   echo "No Redhat version yet."
 fi
 if [ -f /etc/debian_version ]; then
-  apt-get -y update && apt-get install python-apt python-pip build-essential python-dev git
+  apt-get update && apt-get install python-apt python-pip build-essential python-dev git -y
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rackspace-orchestration-templates/lamp
   cd lamp/site-cookbooks/LAMP/files/default/lamp
