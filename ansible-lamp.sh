15240d8dfb193b1dbc35a98df427604f2e90fa72
tbr0 <taylor.broesche@rackspace.com>
2014-08-22 15:22:02 -0500

    Redhat support for ansible-lamp

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,5 +1,9 @@
 if [ -f /etc/redhat-release ]; then
-  echo "No Redhat version yet."
+  yum install -y git
+  pip install paramiko PyYAML jinja2 httplib2 ansible
+  git clone https://github.com/rackspace-orchestration-templates/lamp
+  cd lamp/site-cookbooks/LAMP/files/default/lamp 
+  ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
   apt-get update && apt-get install python-apt python-pip build-essential python-dev git -y
