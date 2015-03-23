e16abea77272675f140eff6739a9f9ef6269c691
tbr0 <t.broesche@gmail.com>
2015-03-23 12:15:12 -0500

    Some new scripts

---
+++ lbgetssltermination.sh
@@ -0,0 +1,5 @@
+region=$1
+token=$2
+account=$3
+loadbalancerid=$4
+curl -skH "X-Auth-Token: ${token}" https://${region}.loadbalancers.api.rackspacecloud.com/v1.0/${account}/loadbalancers/${loadbalancerid}/ssltermination
