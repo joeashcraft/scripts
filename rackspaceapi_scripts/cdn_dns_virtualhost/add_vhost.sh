8b19099f48740e95b0f41f5b590be4d3d3547d41
Taylor <taylor.broesche@rackspace.com>
2015-06-08 16:36:00 -0500

    Deleted some junk.

--- rackspaceapi_scripts/cdn_dns_virtualhost/add_vhost.sh
+++
@@ -1,4 +0,0 @@
-## Usage: add_vhost.sh example.com
-## Does not support subdomains at the moment.
-DOMAIN=${1}
-sed "s/DOMAIN\.COM/${DOMAIN}/g" /etc/httpd/vhost.d/default.template > /etc/httpd/vhost.d/${DOMAIN}.conf
--- rackspaceapi_scripts/cdn_dns_virtualhost/show_cdn_services.sh
+++
@@ -1,2 +0,0 @@
-DOMAIN=${1}
-curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" https://global.cdn.api.rackspacecloud.com/v1.0/959019/services
