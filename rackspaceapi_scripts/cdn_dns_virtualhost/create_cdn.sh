d406d1421f2f2fda379a763addf1bfe9e7c01455
Taylor <taylor.broesche@rackspace.com>
2015-06-08 16:11:31 -0500

    CDN API script working in tests.

--- rackspaceapi_scripts/cdn_dns_virtualhost/create_cdn.sh
+++ rackspaceapi_scripts/cdn_dns_virtualhost/create_cdn.sh
@@ -20,7 +20,7 @@ function create_vhost_from_template_and_domain {
 }
 
 function create_cdn_from_domain {
-  sed s/DOMAIN/${DOMAIN}/g data.template.json | sed s/EMAIL_ADDRESS/${EMAIL_ADDRESS}/g | sed s/IP_ADDRESS/${IP_ADDRESS}/g > data.json
+  sed s/DOMAIN/${DOMAIN}/g data.template.json > data.json
   curl -sX POST -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services -d @data.json
 }
 
@@ -28,13 +28,29 @@ function get_cdn_href_from_domain {
   curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services" | python -mjson.tool | grep ${DOMAIN} | grep href | awk -F: '{print $2}' | tr -d \" | tr -d , | tr -d " "
 }
 
+function get_cdn_href_from_domain_with_marker {
+  curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services?marker=${MARKER}" | python -mjson.tool | grep ${DOMAIN} | grep href | awk -F: '{print $2}' | tr -d \" | tr -d , | tr -d " "
+}
+
 function create_dns_from_cdn_and_domain {
-  sed s/DOMAIN/${DOMAIN}/g domain.template.json | sed s/CDN/${CDNHREF}/g > domain.json
+  sed s/DOMAIN/${DOMAIN}/g domain.template.json | sed s/CDN/${CDNHREF}/g | sed s/EMAIL_ADDRESS/${EMAIL_ADDRESS}/g | sed s/IP_ADDRESS/${IP_ADDRESS}/g > domain.json
   curl -sX POST -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" https://dns.api.rackspacecloud.com/v1.0/${DDI}/domains -d @domain.json &> /dev/null
 }
 
 function get_number_of_cdn_services {
   curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services" | python -mjson.tool | grep "\"id\":" | wc -l  
+}
+
+function get_number_of_cdn_services_with_marker {
+  curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services?marker=${MARKER}" | python -mjson.tool | grep "\"id\":" | wc -l
+}
+
+function get_last_id {
+  curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services" | python -mjson.tool | grep "\"id\":" | awk -F":" '{print $2}' | tr -d " " | tr -d "\"" | tr -d "," | tail -1
+}
+
+function get_last_id_with_marker {
+  curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://global.cdn.api.rackspacecloud.com/v1.0/${DDI}/services?marker=${MARKER}" | python -mjson.tool | grep "\"id\":" | awk -F":" '{print $2}' | tr -d " " | tr -d "\"" | tr -d "," | tail -1
 }
 
 #function optional_post_commands {
@@ -44,10 +60,11 @@ function get_number_of_cdn_services {
 #}
 
 ## Main ##
+#echo "Creating virtual host from template.."
 #create_vhost_from_template_and_domain
 echo "Checking number of current CDN services..."
-NUMBERSERV=get_number_of_cdn_services
-if [ ${NUMBERSERV} != 10 ] then
+NUMBERSERV=`get_number_of_cdn_services`
+if [ ${NUMBERSERV} != 10 ] ; then
   echo "Current services: ${NUMBERSERV}, proceeding as normal."
   echo "Creating CDN for domain: ${DOMAIN}..."
   create_cdn_from_domain
@@ -55,9 +72,27 @@ if [ ${NUMBERSERV} != 10 ] then
   sleep 25
   CDNHREF=`get_cdn_href_from_domain`
   echo "CDN HREF: ${CDNHREF}"
-  echo "Creating DNS entires..."
+  echo "Creating DNS entries..."
   create_dns_from_cdn_and_domain
   echo "Done."
 else
-  echo "${NUMBERSERV} more than 10, need to go for a loop.."
+  echo "CDN services = ${NUMBERSERV}, applying different methodology.."
+  echo "Creating CDN for domain: ${DOMAIN}..."
+  create_cdn_from_domain
+  echo "Waiting 25 seconds for CDN HREF to become available..."
+  sleep 25
+  CDNHREF=`get_cdn_href_from_domain`
+  echo "CDN HREF: ${CDNHREF}"
+  MARKER=`get_last_id`
+  echo ${MARKER}
+  while [ -z ${CDNHREF} ]; do
+    echo "Couldn't find CDN HREF, looping through services.."
+    CDNHREF=`get_cdn_href_from_domain_with_marker`
+    echo ${CDNHREF}
+    MARKER=`get_last_id_with_marker`
+    echo ${MARKER}
+  done
+  echo "CDN HREF: ${CDNHREF}"
+  create_dns_from_cdn_and_domain
+  echo "Done."
 fi
