3aa512f76ade5b4ac1d4c2f12d8c2bae7e1d241a
tbr0 <taylor.broesche@rackspace.com>
2015-06-12 18:48:58 -0500

    Bash comparing and modifying strings. Token tests.

--- wordpress_scripts/wordpress_install/inject_salt.sh
+++
@@ -1,4 +0,0 @@
-SALT=`curl -sk https://api.wordpress.org/secret-key/1.1/salt/`
-STRING='put your unique phrase here'
-printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/vhosts/${DOMAIN}/wp-config.php
-#echo ${WP_SALT}
---
+++ wordpress_scripts/wordpress_install/update_dns_with_domain_name.sh
@@ -0,0 +1,22 @@
+DOMAIN=${1}
+DDI="959019"
+
+## Function assumes domain given is a subdomain, and removes it.
+function get_domain_id_with_domain {
+  F_DOMAIN=`echo ${DOMAIN} | awk -F"." '{print $2"."$3}'`
+  DOMAIN_ID=`curl -sX GET -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://dns.api.rackspacecloud.com/v1.0/${DDI}/domains?name=${F_DOMAIN}" | python -mjson.tool | grep '"id":' | tr -d " " | tr -d "," | awk -F":" '{print $2}'`
+}
+
+function set_record_with_domain_id {
+  curl -sX POST -H "X-Auth-Token: ${TOKEN}" -H "Content-Type: application/json" "https://dns.api.rackspacecloud.com/v1.0/${DDI}/domains/${DOMAIN_ID}/records" -d \
+  "{
+    \"records\" : [ {
+      \"name\" : \"${DOMAIN}\",
+      \"type\" : \"A\",
+      \"data\" : \"162.209.114.197\",
+      \"ttl\" : 300 } ]
+  }" &> /dev/null
+}
+
+get_domain_id_with_domain
+set_record_with_domain_id
