692a7c780a2291e8f9c4bb388b9e8fdfd491ca92
tbr0 <taylor.broesche@rackspace.com>
2015-08-12 16:06:35 -0500

    Tweaking the Magento Nginx virtual host.

--- lbgetssltermination.sh
+++
@@ -1,5 +0,0 @@
-region=$1
-token=$2
-account=$3
-loadbalancerid=$4
-curl -skH "X-Auth-Token: ${token}" https://${region}.loadbalancers.api.rackspacecloud.com/v1.0/${account}/loadbalancers/${loadbalancerid}/ssltermination
