9b2d82297b5bc29d252acbbe7d680d855e6d3966
Taylor Broesche <taylor.broesche@rackspace.com>
2016-03-24 00:57:17 -0500

    Need to modify managed lamp to make work with heat.

--- ansible-lamp2.sh
+++ ansible-lamp2.sh
@@ -1,14 +1,14 @@
 if [ -f /etc/redhat-release ]; then
   yum install -y git python-pip python-devel gcc
   pip install paramiko PyYAML jinja2 httplib2 ansible
-  git clone https://github.com/rackspace-orchestration-templates/lamp
-  cd lamp/site-cookbooks/LAMP/files/default/lamp 
+  git clone git@github.com:tbr0/ansible-managed-lamp.git /tmp/lamp
+  cd /tmp/lamp
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
   apt-get update && apt-get install python-apt python-pip build-essential python-dev git -y
   pip install paramiko PyYAML jinja2 httplib2 ansible
-  git clone https://github.com/rackspace-orchestration-templates/lamp
-  cd lamp/site-cookbooks/LAMP/files/default/lamp
+  git clone git@github.com:tbr0/ansible-managed-lamp.git /tmp/lamp
+  cd /tmp/lamp
   ansible-playbook -i hosts site.yml
 fi 
