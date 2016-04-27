00a7f5882f063aaa1586c45e7aaed26a7e16ea2b
Joe Ashcraft <joeashcraft@gmail.com>
2016-04-27 18:34:44 -0500

    add python-virtualenv to list of packages to install

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,12 +1,12 @@
 if [ -f /etc/redhat-release ]; then
-  yum install -y git python-pip python-devel gcc
+  yum install -y git python-pip python-devel python-virtualenv gcc
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp 
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
-  apt-get update && apt-get install python-apt python-pip build-essential python-dev git -y
+  apt-get update && apt-get install python-apt python-pip build-essential python-dev python-virtualenv git -y
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
