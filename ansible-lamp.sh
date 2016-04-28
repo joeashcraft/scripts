12af35411191d102748ce1ba797f2f7fe968e674
Joe Ashcraft <joeashcraft@gmail.com>
2016-04-28 16:59:51 -0500

    use virtualenv
    I, for one, like installing ansible in a virtualenv

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,12 +1,14 @@
 if [ -f /etc/redhat-release ]; then
   yum install -y git python-pip python-devel python-virtualenv gcc
+  virutalenv /root/lampenv;  cd /root/lampenv; source bin/activate
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp 
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
-  apt-get update && apt-get install python-apt python-pip build-essential python-dev python-virtualenv git -y
+  apt-get update && apt-get -y install python-apt python-pip build-essential python-dev python-virtualenv git
+  virutalenv /root/lampenv;  cd /root/lampenv; source bin/activate
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
