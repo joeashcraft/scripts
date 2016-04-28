70ba0f514d69d617a43815eaa379595328d3b6b4
Joe Ashcraft <joeashcraft@gmail.com>
2016-04-28 17:01:45 -0500

    speling is hard; virutalenv

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,6 +1,6 @@
 if [ -f /etc/redhat-release ]; then
   yum install -y git python-pip python-devel python-virtualenv gcc
-  virutalenv /root/lampenv;  cd /root/lampenv; source bin/activate
+  virtualenv /root/lampenv;  cd /root/lampenv; source bin/activate
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp 
@@ -8,7 +8,7 @@ if [ -f /etc/redhat-release ]; then
 fi
 if [ -f /etc/debian_version ]; then
   apt-get update && apt-get -y install python-apt python-pip build-essential python-dev python-virtualenv git
-  virutalenv /root/lampenv;  cd /root/lampenv; source bin/activate
+  virtualenv /root/lampenv;  cd /root/lampenv; source bin/activate
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
