761f839cb2e3a472ef1ce7339e6cbc23e234d534
tbr0 <t.broesche@gmail.com>
2016-05-27 15:59:07 -0500

    Update ansible-lamp.sh

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -6,7 +6,7 @@ if [ -f /etc/redhat-release ]; then
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
-  apt-get update && apt-get install python-apt python-pip build-essential python-dev git -y
+  apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
   pip install paramiko PyYAML jinja2 httplib2 ansible
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
