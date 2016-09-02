672eaed4afdab912b017f2235c50642828f63694
tbr0 <tbr0@localhost.localdomain>
2016-09-02 14:50:15 -0500

    Adding to bootstrap file.

--- mybootstrap.sh
+++ mybootstrap.sh
@@ -1,3 +1,7 @@
 apvhost () {
     bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/apache-create-virtualhost.sh") $1
 }
+
+kicklamp () {
+    bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/ansible-lamp.sh")
+}
