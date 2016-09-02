99b9098b05621ed7f271b51afc0c4d1ebb8e594f
tbr0 <tbr0@localhost.localdomain>
2016-09-02 14:36:27 -0500

    Fixing bash redirect.

--- mybootstrap.sh
+++ mybootstrap.sh
@@ -1,3 +1,3 @@
 apvhost () {
-    bash<(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/apache-create-virtualhost.sh") $1
+    bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/apache-create-virtualhost.sh") $1
 }
