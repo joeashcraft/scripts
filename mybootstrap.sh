95401ef40596667be31189a555315123ac0c733f
Taylor Broesche <taylor.broesche@rackspace.com>
2016-10-24 11:06:43 -0500

    Updating mybootstrap with trove install.

--- mybootstrap.sh
+++ mybootstrap.sh
@@ -9,3 +9,23 @@ kicklamp () {
 getpass () {
     bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/getpass.sh")
 }
+installtrove () {
+    virtualenv trove;
+    . trove/bin/activate;
+    pip install --upgrade setuptools;
+    pip install python-troveclient rackspace-auth-openstack;
+    echo "DDI:"; read DDI;
+    echo "User:"; read USER;
+    echo "Key:"; read KEY;
+    cat << EOF > .trove
+    export OS_AUTH_URL=https://identity.api.rackspacecloud.com/v2.0/
+    export OS_REGION_NAME=IAD
+    export OS_AUTH_SYSTEM=rackspace
+    export OS_USERNAME=${USER}
+    export OS_TENANT_ID=${DDI}
+    export TROVE_SERVICE_TYPE=rax:database
+    export OS_KEY=${KEY}
+    export OS_PROJECT_ID=${DDI}
+    EOF
+    source .trove
+}
