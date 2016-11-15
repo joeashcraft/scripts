250488f36a261bd83c5cf0fcaad4ffa6fab6d809
Taylor Broesche <taylor.broesche@rackspace.com>
2016-11-15 14:15:44 -0600

    Updating ansible kick.

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -7,7 +7,8 @@ if [ -f /etc/redhat-release ]; then
 fi
 if [ -f /etc/debian_version ]; then
   apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
-  pip install paramiko PyYAML jinja2 httplib2 ansible
+  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe
+  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
   git clone https://github.com/rillip3/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
   ansible-playbook -i hosts site.yml
