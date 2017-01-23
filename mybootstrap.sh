267ff88026e6afb8c31fbd8fec92fd969170e09a
Taylor Broesche <taylor.broesche@rackspace.com>
2017-01-23 12:02:02 -0600

    Adding rack cli install to bootstrap.

--- mybootstrap.sh
+++ mybootstrap.sh
@@ -90,3 +90,7 @@ lsyncdmaas () {
     cd cloud-monitoring-plugin-deploy
     ansible-playbook -i hosts lsyncd_status.yml
 }
+getrackcli () {
+    wget -O /opt/rack "https://ec4a542dbf90c03b9f75-b342aba65414ad802720b41e8159cf45.ssl.cf5.rackcdn.com/1.2/Linux/amd64/rack"
+    ln -s /opt/rack /usr/bin/rack
+}
