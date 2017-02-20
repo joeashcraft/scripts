0da8aa764331f4374d22762619f0fde9fffc9794
Taylor Broesche <taylor.broesche@rackspace.com>
2017-02-20 14:38:14 -0600

    Something strange on 1604 not recognizing ansible modules. Trying this no-binary command for pip.

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,12 +1,12 @@
 if [ -f /etc/redhat-release ]; then
-  yum install -y git python-pip python-devel gcc openssl-devel holland-mysqldump python-virtualenv python-virtualenvwrapper
-  yum -y update
-  source /bin/virtualenvwrapper_lazy.sh
-  mkvirtualenv lampenv
-  pip install pip --upgrade
-  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-  git clone https://github.com/rackerlabs/lamp.git
-  cd lamp/site-cookbooks/LAMP/files/default/lamp
+  yum install -y git python-pip python-devel gcc openssl-devel holland-mysqldump python-virtualenv python-virtualenvwrapper && \
+  yum -y update && \
+  source /bin/virtualenvwrapper_lazy.sh && \
+  mkvirtualenv lampenv && \
+  pip install pip --upgrade && \
+  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade && \
+  git clone https://github.com/rackerlabs/lamp.git && \
+  cd lamp/site-cookbooks/LAMP/files/default/lamp && \
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
@@ -16,7 +16,7 @@ if [ -f /etc/debian_version ]; then
   source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh && \
   mkvirtualenv lampenv && \
   pip install pip --upgrade && \
-  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade && \
+  pip install --no-binary :all: paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade && \
   git clone https://github.com/rackerlabs/lamp.git && \
   cd lamp/site-cookbooks/LAMP/files/default/lamp && \
   ansible-playbook -i hosts site.yml
