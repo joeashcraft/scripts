ecb47613817fd54098d2691b5cd69c9635707c5d
Taylor Broesche <taylor.broesche@rackspace.com>
2017-02-20 12:08:52 -0600

    Just some cleaning up.

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -12,11 +12,10 @@ fi
 if [ -f /etc/debian_version ]; then
   #apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
   apt-get update && apt-get install python-virtualenv virtualenvwrapper libyaml-dev libffi-dev python-pip build-essential python-dev git -y
-  . /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
+  source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
   mkvirtualenv lampenv
   pip install pip --upgrade
   pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-  #git clone https://github.com/rillip3/lamp.git
   git clone https://github.com/rackerlabs/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
   ansible-playbook -i hosts site.yml
