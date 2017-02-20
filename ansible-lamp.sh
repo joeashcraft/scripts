8205266f56d045508500e1714eeece9b394b4b35
Taylor Broesche <taylor.broesche@rackspace.com>
2017-02-20 11:55:55 -0600

    Going back to the curl < bash method.

--- ansible-lamp.sh
+++ ansible-lamp.sh
@@ -1,18 +1,23 @@
 if [ -f /etc/redhat-release ]; then
-  yum install -y git python-pip python-devel gcc openssl-devel
-  #pip install paramiko PyYAML jinja2 httplib2 ansible
+  yum install -y git python-pip python-devel gcc openssl-devel holland-mysqldump
+  yum -y update
   pip install pip --upgrade
   pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-  git clone https://github.com/rillip3/lamp.git
-  cd lamp/site-cookbooks/LAMP/files/default/lamp 
+  #git clone https://github.com/rillip3/lamp.git
+  git clone https://github.com/rackerlabs/lamp.git
+  cd lamp/site-cookbooks/LAMP/files/default/lamp
   ansible-playbook -i hosts site.yml
 fi
 if [ -f /etc/debian_version ]; then
-  apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
+  #apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
+  apt-get update && apt-get install python-virtualenv virtualenvwrapper libyaml-dev libffi-dev python-pip build-essential python-dev git -y
+  . /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
+  mkvirtualenv lampenv
   pip install pip --upgrade
-  pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe
   pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-  git clone https://github.com/rillip3/lamp.git
+  #git clone https://github.com/rillip3/lamp.git
+  git clone https://github.com/rackerlabs/lamp.git
   cd lamp/site-cookbooks/LAMP/files/default/lamp
   ansible-playbook -i hosts site.yml
-fi 
+fi
+
--- mybootstrap.sh
+++ mybootstrap.sh
@@ -3,30 +3,7 @@ apvhost () {
 }
 
 kicklamp () {
-    #bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/ansible-lamp.sh")
-    if [ -f /etc/redhat-release ]; then
-      yum install -y git python-pip python-devel gcc openssl-devel holland-mysqldump
-      yum -y update
-      pip install pip --upgrade
-      pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-      #git clone https://github.com/rillip3/lamp.git
-      git clone https://github.com/rackerlabs/lamp.git
-      cd lamp/site-cookbooks/LAMP/files/default/lamp
-      ansible-playbook -i hosts site.yml
-    fi
-    if [ -f /etc/debian_version ]; then
-      #apt-get update && apt-get install python-markupsafe python-apt libffi-dev python-pip build-essential python-dev git -y
-      apt-get update && apt-get install python-virtualenv virtualenvwrapper libyaml-dev libffi-dev python-pip build-essential python-dev git -y
-      . /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
-      mkvirtualenv lampenv
-      pip install pip --upgrade
-      pip install paramiko PyYAML jinja2 httplib2 ansible==1.6.1 markupsafe --upgrade
-      #git clone https://github.com/rillip3/lamp.git
-      git clone https://github.com/rackerlabs/lamp.git
-      cd lamp/site-cookbooks/LAMP/files/default/lamp
-      ansible-playbook -i hosts site.yml
-    fi
-
+    bash <(curl -s "https://raw.githubusercontent.com/tbr0/scripts/master/ansible-lamp.sh")
 }
 
 getpass () {
